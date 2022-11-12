bd95522a9cf8e1682ac4526ed6d1a1d91ca23063
Ankush-Chander <ankush.watchtower@gmail.com>
2022-11-12 12:20:24 +0530

    add output

--- Chapter9_TheMultiprocessingModule/pi_estimator_python_joblib.py
+++ Chapter9_TheMultiprocessingModule/pi_estimator_python_joblib.py
@@ -49,4 +49,55 @@ if __name__ == '__main__':
     nbr_samples_in_total = 1e8
     joblib_main(nbr_parallel_blocks)
 
-# TODO: Why speedup on 4 workers same as 8 workers
+# >>> python3 -u pi_estimator_python_joblib.py 1
+# making 100000000.0 samples per 1 workers
+# [Parallel(n_jobs=1)]: Using backend SequentialBackend with 1 concurrent workers.
+# executing estimate_nbr_points_in_quarter_circle with 100000000.0 on pid: 952850
+# finished processing 100000000.0 darts at process#952850 in 51.72025775909424
+# [Parallel(n_jobs=1)]: Done   1 out of   1 | elapsed:   51.7s finished
+# estimated pi: 3.1414128
+# Delta: 51.72098112106323
+
+
+# >>> python3 -u pi_estimator_python_joblib.py 4
+# making 25000000.0 samples per 4 workers
+# [Parallel(n_jobs=4)]: Using backend LokyBackend with 4 concurrent workers.
+# executing estimate_nbr_points_in_quarter_circle with 25000000.0 on pid: 952661
+# executing estimate_nbr_points_in_quarter_circle with 25000000.0 on pid: 952659
+# executing estimate_nbr_points_in_quarter_circle with 25000000.0 on pid: 952658
+# executing estimate_nbr_points_in_quarter_circle with 25000000.0 on pid: 952660
+# finished processing 25000000.0 darts at process#952658 in 14.697245597839355
+# finished processing 25000000.0 darts at process#952659 in 14.770778894424438
+# [Parallel(n_jobs=4)]: Done   2 out of   4 | elapsed:   15.1s remaining:   15.1s
+# finished processing 25000000.0 darts at process#952661 in 14.849751472473145
+# finished processing 25000000.0 darts at process#952660 in 14.862541437149048
+# [Parallel(n_jobs=4)]: Done   4 out of   4 | elapsed:   15.2s finished
+# estimated pi: 3.14120364
+# Delta: 15.169500350952148
+
+
+# >>> python3 -u pi_estimator_python_joblib.py 8
+# making 12500000.0 samples per 8 workers
+# [Parallel(n_jobs=8)]: Using backend LokyBackend with 8 concurrent workers.
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953420
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953415
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953416
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953421
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953417
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953418
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953419
+# executing estimate_nbr_points_in_quarter_circle with 12500000.0 on pid: 953422
+# finished processing 12500000.0 darts at process#953417 in 16.571314811706543
+# finished processing 12500000.0 darts at process#953415 in 16.9799485206604
+# [Parallel(n_jobs=8)]: Done   2 out of   8 | elapsed:   17.4s remaining:   52.1s
+# finished processing 12500000.0 darts at process#953422 in 17.139020204544067
+# finished processing 12500000.0 darts at process#953421 in 17.255162000656128
+# finished processing 12500000.0 darts at process#953420 in 17.42788004875183
+# finished processing 12500000.0 darts at process#953419 in 17.40841555595398
+# finished processing 12500000.0 darts at process#953416 in 17.614360332489014
+# finished processing 12500000.0 darts at process#953418 in 17.581160306930542
+# [Parallel(n_jobs=8)]: Done   8 out of   8 | elapsed:   18.0s finished
+# estimated pi: 3.14180008
+# Delta: 18.03565788269043
+
+# TODO: Why speedup on 8 workers same/less than 4 workers
